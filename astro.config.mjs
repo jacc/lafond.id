a3dea700cd548a33349acd28d4d19903699964c1
Jack LaFond <jack.lafond@icloud.com>
2024-10-04 22:28:11 -0400

    nuke half / add half

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,8 +11,11 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     mdx(),
-    db(),
   ],
-  output: "server",
+  output: "static",
   adapter: vercel({ webAnalytics: true }),
+  redirects: {
+    "/blog/shellhacks":
+      "https://www.linkedin.com/posts/jacklafond_shellhacks-hackathon-cybersecurity-activity-7246530359977197568-ezD8",
+  },
 });
--- db/seed.ts
+++
@@ -1,6 +0,0 @@
-import { Views, db } from "astro:db";
-
-// https://astro.build/db/seed
-export default async function seed() {
-  await db.insert(Views).values([{ slug: "tracelabs", count: 1 }]);
-}
